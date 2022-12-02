2bd1dc9712ce2e401bdc775871da14ceeb1ce7cc
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-12-03 01:07:54 +0530

    [Feat]: Completed Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Open Sans"', "sans-serif"],
+    },
+    extend: {
+      colors: {
+        grayPrimary: "#2C2C32",
+        primary: "#0071BC",
+      },
+    },
   },
   plugins: [],
 };
